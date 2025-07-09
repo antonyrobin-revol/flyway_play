338e58d420d51c5b7255119ca7f1387e0280b1de
antonyrobin-revol <antonyrobin@revolsoft.com>
2025-07-09 16:46:57 +0530

    Added migrations

---
+++ Postgres/Postgress_DB/migrations/V002_20250709155351__Robin.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (id, name) VALUES (1, 'John Doe');
+INSERT INTO person (id, name) VALUES (2, 'Jane Smith');
+INSERT INTO person (id, name) VALUES (3, 'Alice Johnson');
+INSERT INTO person (id, name) VALUES (4, 'Bob Brown');
+INSERT INTO person (id, name) VALUES (5, 'Charlie White');
